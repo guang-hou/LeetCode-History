b80ba66fb1462a0b983fe0561d1705518842e55b
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-01-12 14:26:37 -0500

    Time: 150 ms (7.74%), Space: 23.5 MB (17.30%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,38 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next: return None
-        
-        slow, fast = head, head
-        
-        while fast and fast.next:
-            pre = slow
-            fast = fast.next.next
-            slow = slow.next
-        pre.next = None  # cut off the firt half with the rest
-        
-        prev = None
-        cur = slow
+        data = []
+        cur = head
         while cur:
-            post = cur.next
-            cur.next = prev
-            prev = cur
-            cur = post
-        
-        right = prev
-        left = head
-        
-        while left:
-            if left.next:
-                left_post = left.next
-                right_post = right.next
-                left.next = right
-                right.next = left_post
-                left = left_post
-                right = right_post
-            else:
-                left.next = right
-                break
+            data.append(cur.val)
+            cur = cur.next
+            
+        cur = head
+        left, right = 1, len(data) - 1
+        while left <= right:
+            right_node = ListNode(data[right])
+            cur.next = right_node
+            right -= 1
+            cur = cur.next
+            
+            if left <= right:
+                left_node = ListNode(data[left])
+                cur.next = left_node
+                cur = cur.next
+                left += 1
         
             
         
