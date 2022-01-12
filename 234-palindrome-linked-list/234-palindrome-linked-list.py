445abd72a6997d3060066a126ec313d9fe0450ce
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-01-12 11:33:40 -0500

    Time: 704 ms (95.01%), Space: 31.4 MB (98.79%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,7 +5,6 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
         length = 0
         
         cur = head
@@ -13,24 +12,34 @@ class Solution:
             length += 1
             cur = cur.next
         
-        cur = head
-        for _ in range(1, length // 2 + 1):
-            stack.append(cur.val)
-            cur = cur.next
+        if length == 1:
+            return True
+        elif length == 2 and head.val != head.next.val:
+            return False
+        elif length == 3 and head.val == head.next.next.val:
+            return True
         
-        if length % 2 == 0:
-            right_start = length // 2 + 1
+        pre, cur = None, head
+        
+        for _ in range(0, length // 2):
+            post = cur.next
+            cur.next = pre
+            pre = cur
+            cur = post
+            
+        left_start = pre
+        
+        if length % 2 == 1:
+            right_start = cur.next
         else:
-            right_start = length // 2 + 2
-            cur = cur.next
+            right_start = cur
         
-        while stack:
-            if stack[-1] != cur.val:
+        while right_start:
+            if right_start.val != left_start.val:
                 return False
             else:
-                stack.pop()
-                cur = cur.next
-        
+                right_start = right_start.next
+                left_start = left_start.next
         return True
         
         
