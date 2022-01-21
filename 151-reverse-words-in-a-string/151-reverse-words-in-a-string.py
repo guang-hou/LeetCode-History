239d6d01444475e756dac3c83f4ef718f4f03de5
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-01-21 07:50:19 -0500

    Time: 60 ms (17.99%), Space: 14.3 MB (68.24%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -2,36 +2,20 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         s = list(s)
         
-        left, right = 0, len(s) - 1
+        word = []
+        words = []
         
-        while s[left].isspace():
-            left += 1
-        while s[right].isspace():
-            right -= 1
+        for i in range(len(s) - 1):
+            if s[i] != ' ':
+                word.append(s[i])
+                if s[i + 1] == ' ':
+                    words.append(''.join(word))
+                    word = []
+                    
+        if word or s[-1] != ' ': 
+            word.append(s[-1])
+            words.append(''.join(word))
+            
+        words.reverse()
         
-        s = s[left:right + 1]
-        res = []
-        
-        for i in range(len(s)):
-            if not s[i].isspace():
-                res.append(s[i])
-            else:
-                if not s[i + 1].isspace():
-                    res.append(s[i])
-                else:
-                    continue
-        
-        results = []
-        tmp = []
-        for i in range(len(res)):
-            if not res[i].isspace() or i == len(s) - 1:
-                tmp.append(res[i])
-            else:
-                results.append("".join(tmp))
-                results.append(" ")
-                tmp = []
-                
-        results.append("".join(tmp))
-        results.reverse()
-        
-        return "".join(results)
+        return " ".join(words)
