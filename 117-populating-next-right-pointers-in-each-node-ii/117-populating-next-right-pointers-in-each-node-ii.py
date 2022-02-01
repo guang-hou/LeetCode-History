2879ba0dbea2211ae84fa30775e9e97a22ae1683
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-01 15:03:52 -0500

    Time: 113 ms (5.13%), Space: 15.3 MB (80.42%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -11,17 +11,18 @@ class Node:
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
         if not root: return None
-        
-        dq = deque([root])
-        
-        while dq:
-            n = len(dq)
-            
-            for i in range(n):
-                cur = dq.popleft()
-                if i < n - 1: cur.next = dq[0]
-            
-                if cur.left: dq.append(cur.left)
-                if cur.right: dq.append(cur.right)
-         
+        first = root
+        while first:  # 遍历每一层
+            dummyHead = Node(None)  # 为下一行创建一个虚拟头节点，相当于下一行所有节点链表的头结点(每一层都会创建)；
+            tail = dummyHead  # 为下一行维护一个尾节点指针（初始化是虚拟节点）
+            cur = first
+            while cur:  # 遍历当前层的节点（假设当前层所有节点已经连接好）, 处理下一层节点
+                if cur.left:  # 链接下一行的节点
+                    tail.next = cur.left
+                    tail = cur.left
+                if cur.right:
+                    tail.next = cur.right
+                    tail = cur.right
+                cur = cur.next  # cur同层移动到下一节点
+            first = dummyHead.next  # 此处为换行操作，更新到下一行
         return root
