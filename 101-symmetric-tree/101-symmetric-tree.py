e350fd53890853371ec7480ca9f2b911c0477544
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-05 09:18:25 -0500

    Time: 45 ms (46.27%), Space: 13.8 MB (99.50%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,31 +6,21 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root.left and not root.right: return True
-        elif root.left and not root.right: return False
-        elif not root.left and root.right: return False
         
-        stack1 = [root.left]  # SLR
-        stack2 = [root.right] # SRL
+        stack = [root.left, root.right]  
         
-        while stack1 and stack2:
-            cur1 = stack1.pop()
-            cur2 = stack2.pop()
-            if cur1.val != cur2.val:
+        while stack:
+            cur1 = stack.pop()
+            cur2 = stack.pop()
+            if cur1 and not cur2 or not cur1 and cur2:
                 return False
-            if (cur1.right and not cur2.left) or (cur1.left and not cur2.right):
-                return False
-            elif cur1.right and cur2.left and cur1.right.val != cur2.left.val:
-                return False
-            elif cur1.left and cur2.right and cur1.left.val != cur2.right.val:
-                return False
+            elif cur1 and cur2:
+                if cur1.val != cur2.val:
+                    return False
+                else:
+                    stack.append(cur1.left)
+                    stack.append(cur2.right)
+                    stack.append(cur1.right)
+                    stack.append(cur2.left)
             
-            if cur1.right: stack1.append(cur1.right)
-            if cur1.left: stack1.append(cur1.left)
-            
-            if cur2.left: stack2.append(cur2.left)
-            if cur2.right: stack2.append(cur2.right)
-            
-        
-        if not stack1 and not stack2: return True
-        else: return False
+        return True
