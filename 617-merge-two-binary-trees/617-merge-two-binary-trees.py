c1592e0e065ffc0e3d9604a5b1eaa367f22b00ea
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-07 16:07:49 -0500

    Time: 149 ms (20.41%), Space: 15.5 MB (61.17%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -10,33 +10,20 @@ class Solution:
         elif not root1 and root2: return root2
         elif root1 and not root2: return root1
         
-#         stack = [root1, root2]
+        stack = [root1, root2]
         
-#         while stack:
-#             cur2 = stack.pop()
-#             cur1 = stack.pop()
+        while stack: # will not add None into stack
+            cur2 = stack.pop()
+            cur1 = stack.pop()
             
-#             if cur2 and cur1:
-#                 cur1.val += cur2.val
-#             elif cur2 and not cur1:
+            cur1.val += cur2.val
+            
+            if cur2.right:
+                if not cur1.right: cur1.right = TreeNode(0)
+                stack.extend([cur1.right, cur2.right])
+            
+            if cur2.left:
+                if not cur1.left: cur1.left = TreeNode(0)
+                stack.extend([cur1.left, cur2.left])
                 
-        def traverse(root1, root2):
-            if not root2: return None      
-            
-            root1.val += root2.val
-            
-            if root2.right: 
-                if not root1.right:
-                    root1.right = TreeNode(0)
-                    
-                traverse(root1.right, root2.right)
-            
-            if root2.left: 
-                if not root1.left: 
-                    root1.left = TreeNode(0)
-                    
-                traverse(root1.left, root2.left)
-        
-        traverse(root1, root2)
-        
         return root1
