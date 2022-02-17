bcf5b10d8fe14824c14776225cadcb187d61d98b
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-17 11:30:00 -0500

    Time: 36 ms (61.04%), Space: 14.1 MB (58.40%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0: return None
+        
+        mapping = {'2': ["a", "b", "c"], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
+                   '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], 
+                   '9': ['w', 'x', 'y', 'z']}
+        
+        res = []
+        path = []
+        
+        def backtrack(start, end):
+            if len(path) == len(digits):
+                res.append("".join(path[:]))
+                return
+            
+            for i in range(start, end - (len(digits) - len(path) - 1)):
+                letters = mapping[digits[i]]
+                for letter in letters:
+                    path.append(letter)                        
+                    backtrack(start + 1, end) # recursion
+                    path.pop()
+        
+        backtrack(0, len(digits))
+                           
+        return res
