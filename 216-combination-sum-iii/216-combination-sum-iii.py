fa2a78584f716f08827879fef847841f2b3fecb9
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-17 10:53:20 -0500

    Time: 48 ms (39.43%), Space: 14 MB (76.42%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -3,16 +3,16 @@ class Solution:
         res = []
         path = []
         
-        def backtrack(start, end, k, n):
-            if len(path) == k and sum(path) == n:
+        def backtrack(k, n, startIndex):
+            if sum(path) == n and len(path) == k:
                 res.append(path[:])
                 return
             elif len(path) < k:
-                for j in range(start, end - (k - len(path) - 1) + 1):
-                    path.append(j)
-                    backtrack(j + 1, end, k, n)
-                    path.pop()
+	            for i in range(startIndex, 9 - (k - len(path) - 1) + 1):
+	                path.append(i)
+	                backtrack(k, n, i + 1)
+	                path.pop()
                 
-        backtrack(1, 9, k, n)
+        backtrack(k, n, 1)
         
         return res
