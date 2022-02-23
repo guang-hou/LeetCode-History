ccb9e25c325830eef1f037e2ae5b026849d356b9
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-02-22 19:31:02 -0500

    Time: 137 ms (33.10%), Space: 14 MB (77.04%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        path = []
+        candidates.sort()
+        
+        def backtrack(i):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            elif sum(path) < target:
+                for j in range(i, len(candidates)):
+                    if j > i and candidates[j] == candidates[j - 1]:
+                        continue
+                    path.append(candidates[j])
+                    backtrack(j)
+                    path.pop()
+            else:
+                return
+        
+        backtrack(0)
+        return res
