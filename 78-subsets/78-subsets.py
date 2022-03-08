0610b50289723486d2b6181fb7ff6f49b0770b65
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-03-08 16:09:19 -0500

    Time: 69 ms (10.08%), Space: 14.2 MB (25.25%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        res = []
+        
+        def backtrack(startIndex):
+            res.append(path[:])
+            
+            for i in range(startIndex, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1)
+                path.pop()
+                
+        backtrack(0)
+        
+        return res
