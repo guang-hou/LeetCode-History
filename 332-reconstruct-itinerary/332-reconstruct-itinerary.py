b47a6420249596a1a62d49ecbe0276b4237a0508
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-03-18 11:29:04 -0400

    Time: 115 ms (56.63%), Space: 14.4 MB (86.42%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        combined_tickets = defaultdict(list)
+        
+        for start, end in tickets:
+            combined_tickets[start].append(end)
+        
+        for start, ends in combined_tickets.items():
+            ends.sort()
+        
+        path, res = ["JFK"], []
+        
+        def backtrack(start):
+            if len(path) == len(tickets) + 1:
+                res.append(path[:])
+                return
+            
+            if res:
+                return
+            
+            if start not in combined_tickets:
+                return
+            
+            ends = combined_tickets[start]
+            for i in range(len(ends)):
+                end = ends[i]
+                path.append(end)
+                ends.pop(i)
+                backtrack(end)
+                ends.insert(i, end)
+                path.pop()
+        
+        backtrack("JFK")
+        
+
+        return res[0]
