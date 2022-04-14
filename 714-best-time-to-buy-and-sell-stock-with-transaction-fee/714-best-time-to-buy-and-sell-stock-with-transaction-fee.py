29f904bf551d5884b4543e999e7c21ee7e638b75
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-04-14 11:06:14 -0400

    Time: 957 ms (61.24%), Space: 23.1 MB (29.07%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        dp = [[0] * 2 for i in range(len(prices))] # 0: no stock, 1: stock
-        
-        dp[0] = [0, -prices[0]]
-        
-        for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
-            dp[i][1] = max(dp[i - 1][0] - prices[i], dp[i - 1][1])
-        
-        print(dp)
-        return max(dp[-1])
+    
+        length = len(prices)
+        dp = [[0] * 2 for _ in range(length)]  # [天数][是否持有股票]
+        dp[0][0] = 0                    # dp[i][0] 表示第i天不持有股票所得现金
+        dp[0][1] = -prices[0]           # dp[i][1] 表示第i天持有股票所得最多现金
+        for i in range(1, length):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee) 
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+        return max(dp[-1][0], dp[-1][1])
