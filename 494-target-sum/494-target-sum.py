930e7418a8fa91741a00ef01d90d9410b10ecb67
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-05-03 21:48:32 -0400

    Time: 75 ms (99.54%), Space: 13.8 MB (98.43%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,23 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         sum_nums = sum(nums)
-        if target > sum_nums or target < -sum_nums:  # 背包的大小应该可以容纳[-sum(nums),sum(nums)]这个区间的所有数
+        if target > sum_nums or target < -sum_nums or (target + sum_nums) % 2 == 1:  
             return 0
                     
-        len_nums = len(nums)
+        volume = (target + sum_nums) // 2
         
-        row, col = len(nums) + 1, 2*sum_nums + 1   # 有len(nums)个元素, 这里用了len(nums) + 1 行
+        dp = [0] * (volume + 1) 
         
-        dp = [[0] * (col) for _ in range(row)]  
+        dp[0] = 1
         
-        dp[0][sum_nums] = 1
+        for num in nums:
+            for i in range(volume, num - 1, -1):
+                dp[i] += dp[i - num]
         
-        for i in range(1, row):
-            for j in range(col):
-                if j + nums[i - 1] < col:
-                    dp[i][j] += dp[i - 1][j + nums[i - 1]]
-                if j - nums[i - 1] >= 0:
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]]
-        
-        return dp[row - 1][sum_nums + target]
+        return dp[volume]
         
