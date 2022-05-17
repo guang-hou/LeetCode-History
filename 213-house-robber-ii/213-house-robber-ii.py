fc5bbc017d7ff0108949b147d4e4eb33c6f5d43b
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-05-17 10:25:27 -0400

    Time: 38 ms (69.48%), Space: 14 MB (25.53%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,26 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
+        if len(nums) == 1:
             return nums[0]
-        elif len(nums) == 1:
+        elif len(nums) == 2:
             return max(nums)
         
-        dp1 = [[0, 0] for _ in range(len(nums))] # rob house 0
+        dp1 = [0] * len(nums) # rob house 0
         
-        dp1[0] = [nums[0], nums[0]]
-        dp1[1] = [nums[0], nums[0]]
+        dp1[0] = nums[0]
+        dp1[1] = nums[0]
         
         for i in range(2, len(nums) - 1):
-            dp1[i][0] = max(dp1[i - 1])
-            dp1[i][1] = dp1[i - 1][0] + nums[i]
+            dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i])
         
-        dp2 = [[0, 0] for _ in range(len(nums))] # not rob house 0
+        dp2 = [0] * len(nums) # not rob house 0
         
-        dp2[0] = [0, 0]
-        dp2[1] = [0, nums[1]]
+        dp2[0] = 0
+        dp2[1] = nums[1]
         
         for i in range(2, len(nums)):
-            dp2[i][0] = max(dp2[i - 1])
-            dp2[i][1] = dp2[i - 1][0] + nums[i]
+            dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i])
         
-        return max(max(dp1[-2]), max(dp2[-1]))
+        return max(dp1[-2], dp2[-1])
