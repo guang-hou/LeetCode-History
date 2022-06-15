d9b6987b6dc17dc7469c8bf631aa7553e93ebbf2
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-06-15 15:54:48 -0400

    Time: 4868 ms (12.30%), Space: 30.5 MB (69.82%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -4,21 +4,20 @@ class Solution:
         res = 1
         
         for i in range(len(s) - 1, -1, -1):
-            for j in range(len(s)):
-                if j >= i:
-                    if i == j:
+            for j in range(i, len(s)):
+                if i == j:  # 矩陣的斜綫
+                    dp[i][j] = 1
+                elif j == i + 1: # 矩陣斜綫平行的右上一條綫
+                    if s[i] == s[j]:
+                        dp[i][j] = 2
+                    else:
                         dp[i][j] = 1
-                    elif j == i + 1:
-                        if s[i] == s[j]:
-                            dp[i][j] = 2
-                        else:
-                            dp[i][j] = 1
-                    elif i < len(s) - 1 and j > i + 1 and j > 0:
-                        if s[i] == s[j]:
-                            dp[i][j] = max(dp[i + 1][j], dp[i][j - 1], dp[i + 1][j - 1] + 2)
-                        else:
-                            dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
-                            
-                    res = max(res, dp[i][j])
+                else: # 右上其他區域，自然會保證不會out of bound
+                    if s[i] == s[j]:
+                        dp[i][j] = max(dp[i + 1][j], dp[i][j - 1], dp[i + 1][j - 1] + 2)
+                    else:
+                        dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+                res = max(res, dp[i][j])
         
         return res
