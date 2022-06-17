d6f6825d54b1cd12089f1de7d53004c7e8fcd139
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-06-17 15:26:13 -0400

    Time: 1171 ms (71.72%), Space: 29.8 MB (36.94%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,31 +1,21 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        size = len(heights)
+        heights.append(0)  # 加入一个小的值，那么原来队列中的值都会被弹出得到计算
+        m = len(heights)
+        stack = [0]
         res = 0
         
-        stack = [0]
-        for i in range(1, size):
+        for i in range(1, m):
             while stack and heights[i] < heights[stack[-1]]:
-                middle = stack.pop()
-                if stack:
-                    left = stack[-1]
-                else:
-                    left = -1
-                area = heights[middle] * (i - left - 1)
+                idx = stack.pop()  # 计算高度列的index
+                right_idx = i
+                
+                if stack: 
+                    left_idx = stack[-1] # handle the case where thiere is no smaller element
+                else: 
+                    left_idx = -1
+                area = (right_idx - left_idx - 1) * heights[idx]
                 res = max(res, area)
-            
             stack.append(i)
-        
-        while stack:
-            right = size
-            middle = stack.pop()
-            if stack:
-                left = stack[-1]
-            else:
-                left = -1
-            area = heights[middle] * (right - left - 1)
-            res = max(res, area)
-        
+
         return res
-            
-        
