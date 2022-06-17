b1956ef8251c269c57a660d38f28ec4e8e330db7
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-06-17 12:46:06 -0400

    Time: 195 ms (32.95%), Space: 16 MB (25.76%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -3,18 +3,23 @@ class Solution:
         if len(height) < 3:
             return 0
         
-        max_left = [height[0]] * len(height)    # i left side's max height
-        max_right = [height[-1]] * len(height)  # i right sides' max height
+        stack = [0]
+        res = 0
         
         for i in range(1, len(height)):
-            max_left[i] = max(height[i], max_left[i - 1])
+            while stack and height[i] > height[stack[-1]]:
+                middle = stack.pop()
+                if stack:
+                    left = stack[-1]
+                    res += (min(height[i], height[left]) - height[middle]) * (i - left - 1)
+            if stack and height[i] == height[stack[-1]]:
+                stack.pop()
         
-        for j in range(len(height) - 2, -1, -1):
-            max_right[j] = max(height[j], max_right[j + 1])
-        
-        res = 0
-        for k in range(1, len(height) - 1):
-            res += min(max_left[k], max_right[k]) - height[k]
-        
+            stack.append(i)
+            
         return res
+                    
+                
+                
+                
         
