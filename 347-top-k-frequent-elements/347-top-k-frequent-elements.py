7621b5d7f6890637a4c04e193f7c4b92ecd008f6
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-07-16 17:56:58 -0400

    Time: 154 ms (55.85%), Space: 19.6 MB (25.29%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,18 +1,19 @@
-from queue import PriorityQueue
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # record frequency : [keys]
         # ferquency will be between 0 and len(nums) + 1
+        count = {}
         frq = [[] for _ in range(len(nums) + 1)]
         
-        for key, cnt in Counter(nums).items():
-            frq[cnt].append(key)
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        
+        for num, cnt in count.items():
+            frq[cnt].append(num)
 
         res = []
         for cnt in range(len(nums), -1, -1):
             keys = frq[cnt]
             res.extend(frq[cnt])
-            if len(res) >= k: return res[:k]
-
-        #return res[:k]
+            if len(res) >= k: 
+                return res[:k]
