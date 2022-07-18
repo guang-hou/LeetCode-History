d791285c4ffd0e072cac8ac9071950cdbb0f5e93
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-07-18 11:11:41 -0400

    Time: 1348 ms (81.54%), Space: 25.2 MB (29.90%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -3,14 +3,23 @@ class Solution:
         res = [0] * len(temperatures)
         stack = [0]
         
-        for j in range(1, len(temperatures)):
-            if temperatures[j] <= temperatures[stack[-1]]:
-                stack.append(j)
+        # for j in range(1, len(temperatures)):
+        #     if temperatures[j] <= temperatures[stack[-1]]:
+        #         stack.append(j)
+        #     else:
+        #         while stack and temperatures[j] > temperatures[stack[-1]]:
+        #             idx = stack.pop()
+        #             res[idx] = j - idx
+        #         stack.append(j)
+        
+        j = 1
+        while j < len(temperatures):
+            if stack and temperatures[j] > temperatures[stack[-1]]:
+                idx = stack.pop()
+                res[idx] = j - idx
             else:
-                while stack and temperatures[j] > temperatures[stack[-1]]:
-                    idx = stack.pop()
-                    res[idx] = j - idx
                 stack.append(j)
+                j += 1
         
         return res
                 
