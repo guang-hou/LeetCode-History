583be5976420292766d3cc98f75613a8951be3af
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-07-18 15:26:28 -0400

    Time: 1376 ms (61.91%), Space: 27.6 MB (95.68%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,15 +1,27 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.insert(0, 0)
-        heights.append(0)  # 这样left_index 一直在，stack不会变为空
-        stack = [0]
-        result = 0
-        for i in range(1, len(heights)):
-            while heights[i] < heights[stack[-1]]:
-                mid_height = heights[stack[-1]]
-                stack.pop()
-                # area = width * height
-                area = (i - stack[-1] - 1) * mid_height
-                result = max(area, result)
+        n = len(heights)
+        left_smaller = [-1] * n  # store the index of left first smaller element
+        right_smaller = [n] * n  # store the index of right first smaller element
+        
+        # calculate right_smaller
+        stack = []
+        for i in range(n):
+            while stack and heights[i] < heights[stack[-1]]:
+                right_smaller[stack.pop()] = i
             stack.append(i)
-        return result
+          
+        # calculate left_smaller
+        stack = []
+        for i in range(n - 1, -1, -1):
+            while stack and heights[i] < heights[stack[-1]]:
+                left_smaller[stack.pop()] = i
+            stack.append(i)
+            
+
+        res = 0
+        for i in range(n):
+            area = heights[i] * (right_smaller[i] - left_smaller[i] - 1)
+            res = max(res, area)
+        
+        return res
