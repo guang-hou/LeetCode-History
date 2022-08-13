bee9a585617b38310a6c019dd03c9058706c6e03
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-08-13 15:09:36 -0400

    Time: 979 ms (53.22%), Space: 29 MB (87.45%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -14,10 +14,10 @@ class Solution:
             self.res.append(self.path[:])
             return        
         
-        for i in range(nodeStartIndex + 1, len(s) + 1):
-            if self.isParlindrome(s, nodeStartIndex, i):
-                self.path.append(s[nodeStartIndex:i])
-                self.dfs(s, i)
+        for i in range(nodeStartIndex, len(s)):
+            if self.isParlindrome(s, nodeStartIndex, i + 1):
+                self.path.append(s[nodeStartIndex:i + 1])
+                self.dfs(s, i + 1)
                 self.path.pop()
             
     # Check if s[start:end] is a parlindrome, not including s[end]
