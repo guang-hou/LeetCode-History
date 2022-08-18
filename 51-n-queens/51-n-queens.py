8e7e2d3f270f047c3b71334fa02f4619730cb64a
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-08-18 11:01:40 -0400

    Time: 208 ms (17.97%), Space: 14.4 MB (46.51%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -6,13 +6,11 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         self.board = [['.'] * n for _ in range(n)]
         
-        #print(self.board)
-        
         self.dfs(0, n)
         
         return self.res
         
-        
+    
     def dfs(self, row, n):
         if row == n:
             self.res.append(["".join(row) for row in self.board])
@@ -33,28 +31,8 @@ class Solution:
         for j in range(n):
             if j < col and self.board[row][j] == 'Q':
                 return False
-        
-        i, j = row, col
-        while i < n and j > -1:
-            if self.board[i][j] == 'Q':
-                return False
-            i += 1
-            j -= 1
-        
-        i, j = row, col
-        while i > -1 and j < n:
-            if self.board[i][j] == 'Q':
-                return False
-            i -= 1
-            j += 1
-            
-        i, j = row, col
-        while i < n and j < n:
-            if self.board[i][j] == 'Q':
-                return False
-            i += 1
-            j += 1
-        
+
+		## 左上角
         i, j = row, col
         while i > -1 and j > -1:
             if self.board[i][j] == 'Q':
@@ -62,7 +40,28 @@ class Solution:
             i -= 1
             j -= 1
         
+		## 右上角
+        i, j = row, col
+        while i > -1 and j < n:
+            if self.board[i][j] == 'Q':
+                return False
+            i -= 1
+            j += 1
+
+		## 左下角, 其实不用判断, 因为row下面的行还没visit,肯定没有Q
+        # i, j = row, col
+        # while i < n and j > -1:
+        #     if self.board[i][j] == 'Q':
+        #         return False
+        #     i += 1
+        #     j -= 1
+            
+		## 右下角, 其实不用判断, 因为row下面的行还没visit,肯定没有Q
+        # i, j = row, col
+        # while i < n and j < n:
+        #     if self.board[i][j] == 'Q':
+        #         return False
+        #     i += 1
+        #     j += 1
+        
         return True
-        
-        
-        
