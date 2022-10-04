58ae4f1a080677b6f0325006453cfb13c09ff2d8
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-10-04 14:41:52 -0400

    Time: 2926 ms (29.37%), Space: 60.1 MB (12.93%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -3,15 +3,14 @@ class Solution:
         points.sort(key=lambda x : x[0])
         res = 1
         
-        leftEdge, rightEdge = points[0]
+        rightEdge = points[0][1]
         
         for i in range(1, len(points)):
             x_start, x_end = points[i]
             if x_start > rightEdge:
-                leftEdge, rightEdge = x_start, x_end
+                rightEdge = x_end
                 res += 1
             else:
-                leftEdge = max(leftEdge, x_start)
                 rightEdge = min(rightEdge, x_end)
         
         return res
