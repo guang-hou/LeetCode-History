567409c3173cfcaad1694c7cb26bd0d9abc5b59a
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-10-10 21:48:56 -0400

    Time: 623 ms (11.99%), Space: 23.9 MB (31.77%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -3,13 +3,17 @@ class Solution:
         n = len(prices)
         
         dp = [[[0, 0] for col in range(k + 1)] for row in range(n)]
-        
-        for j in range(k + 1):
-            dp[0][j][1] = -prices[0]
-        
-        for i in range(1, n):
-            for j in range(1, k + 1):  
-                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i])
-                dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i])
+
+        for i in range(n):
+            for j in range(k + 1):  
+                if j == 0:
+                    dp[i][j][0] = 0
+                    dp[i][j][1] = float("-inf")
+                elif i == 0:
+                    dp[i][j][0] = 0
+                    dp[i][j][1] = -prices[0]
+                else:
+                    dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i])
+                    dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i])
                 
         return dp[-1][k][0]
