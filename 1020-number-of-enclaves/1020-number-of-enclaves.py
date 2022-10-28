a0d9b125293562f616f9b4b3315ff09b6d5b3535
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-10-28 14:30:32 -0400

    Time: 2351 ms (11.81%), Space: 78.2 MB (38.14%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        res = 0
+        
+        for i in range(m):
+            self.dfs(i, 0, grid, visited)
+            self.dfs(i, n - 1, grid, visited)
+            
+        for j in range(n):
+            self.dfs(0, j, grid, visited)
+            self.dfs(m - 1, j, grid, visited)
+            
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    res += self.dfs(i, j, grid, visited)
+        
+        return res
+        
+    def dfs(self, i, j, grid, visited):
+        m, n = len(grid), len(grid[0])
+        res = 0
+        
+        if i < 0 or i == m or j < 0 or j == n:
+            return 0
+        
+        if grid[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True
+            res += 1
+            res += self.dfs(i - 1, j, grid, visited)
+            res += self.dfs(i + 1, j, grid, visited)
+            res += self.dfs(i, j - 1, grid, visited)
+            res += self.dfs(i, j + 1, grid, visited)
+            
+        return res
