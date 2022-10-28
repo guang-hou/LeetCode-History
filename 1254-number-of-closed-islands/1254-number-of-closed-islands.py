d8391542844ed00209c08dac4cba49a36d62ad9f
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-10-28 14:10:37 -0400

    Time: 280 ms (51.76%), Space: 14.4 MB (64.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -5,29 +5,31 @@ class Solution:
         res = 0
         
         for i in range(m):
+            self.dfs(grid, i, 0, visited)
+            self.dfs(grid, i, n - 1, visited)   
+            
+        for j in range(n):
+            self.dfs(grid, 0, j, visited)
+            self.dfs(grid, m - 1, j, visited)
+            
+        for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0 and not visited[i][j]:
-                    if(self.dfs(grid, i, j, visited, m, n) == 0):
-                        res += 1
+                    res += 1
+                    self.dfs(grid, i, j, visited)
         
         return res
+    
         
-    def dfs(self, grid, i, j, visited, m, n):
+    def dfs(self, grid, i, j, visited):
+        m, n = len(grid), len(grid[0])
         if (i < 0 or i >= m or j < 0 or j >= n):
             return 0
         
         if grid[i][j] == 0 and not visited[i][j]:
             visited[i][j] = True
             
-            l = self.dfs(grid, i - 1, j, visited, m, n)
-            r = self.dfs(grid, i + 1, j, visited, m, n)
-            u = self.dfs(grid, i, j - 1, visited, m, n)
-            d = self.dfs(grid, i, j + 1, visited, m, n)
-            
-            if (i == 0 or i == m - 1 or j == 0 or j == n - 1):
-                return -1
-            
-            if (l + r + u + d) != 0:
-                return -1
-            
-        return 0
+            self.dfs(grid, i - 1, j, visited)
+            self.dfs(grid, i + 1, j, visited)
+            self.dfs(grid, i, j - 1, visited)
+            self.dfs(grid, i, j + 1, visited)
