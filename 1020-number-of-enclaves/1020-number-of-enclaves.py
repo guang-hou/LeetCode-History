1579c878988277d7cfe468c04f5319e9e903de47
Guang-Hou <87743934+Guang-Hou@users.noreply.github.com>
2022-10-28 14:34:51 -0400

    Time: 1987 ms (24.32%), Space: 76.5 MB (43.12%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -15,23 +15,21 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1 and not visited[i][j]:
-                    res += self.dfs(i, j, grid, visited)
+                    res += 1
         
         return res
         
     def dfs(self, i, j, grid, visited):
         m, n = len(grid), len(grid[0])
-        res = 0
         
         if i < 0 or i == m or j < 0 or j == n:
             return 0
         
         if grid[i][j] == 1 and not visited[i][j]:
             visited[i][j] = True
-            res += 1
-            res += self.dfs(i - 1, j, grid, visited)
-            res += self.dfs(i + 1, j, grid, visited)
-            res += self.dfs(i, j - 1, grid, visited)
-            res += self.dfs(i, j + 1, grid, visited)
+
+            self.dfs(i - 1, j, grid, visited)
+            self.dfs(i + 1, j, grid, visited)
+            self.dfs(i, j - 1, grid, visited)
+            self.dfs(i, j + 1, grid, visited)
             
-        return res
